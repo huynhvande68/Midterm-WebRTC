a90d1dfe2db7b1f0826810ecaf474ede806fe519
BlackWolf2002 <86212381+hynvande@users.noreply.github.com>
2023-04-27 21:06:20 +0700

    all

---
+++ server.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
+const opinions = {
+  debug: true,
+}
+
+app.use("/peerjs", ExpressPeerServer(server, opinions));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+
+
+// const io = socketio(httpServer);
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    setTimeout(()=>{
+      socket.to(roomId).broadcast.emit("user-connected", userId);
+    }, 1000)
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+  });
+});
+
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log('http://localhost:'+PORT);
+})
